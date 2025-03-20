cd47ff307481a0052d9697474d201fe8ea2b851d
YanBoChen <chen.yanbo@northeastern.edu>
2025-03-20 15:44:41 -0700

    Implement routing for guest login and home page with authentication check

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+// This is the main entry point for the client application. (main page)
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
